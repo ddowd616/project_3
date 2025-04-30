c363bf9b531c94817f458804a58300189a869ffe
Chris Lee <christopher.lee121.mil@army.mil>
2025-04-30 14:50:15 -0500

    add passing test for carService.ts

--- frontend/src/components/CarService.ts
+++ frontend/src/components/CarService.ts
@@ -2,9 +2,9 @@ import {Car} from "../types.ts";
 
 export function fetchCars(): Promise<Car[]>{
     const mockedCars: Car[] = [
-        { make: 'toyota', model: 'corolla', id: 1, year: 2022, used:true, price:12000},
-        { make: 'honda', model: 'civic', id: 2, year: 2020, used:false, price:30000 },
-        { make: 'ford', model: 'F150', id: 3, year: 2019, used:true, price:18000 }
+        {id: 1, make: 'Ford', model: 'Mustang', year: 2017, price: 30000.65, used: true},
+        {id: 2, make: 'Kia', model: 'Optima', year: 2020, price: 13500, used: true},
+        {id: 3, make: 'Chevy', model: 'Silverado', year: 2025, price: 60000.34, used: false},
     ]
     return Promise.resolve(mockedCars)
 }
---
+++ frontend/src/components/__tests__/CarService.test.tsx
@@ -0,0 +1,27 @@
+import {afterAll, afterEach, beforeAll, describe, expect, it} from "vitest";
+import {Car} from "../../types.ts";
+import axios from "axios";
+import {setupServer} from "msw/node";
+import {http, HttpResponse} from "msw";
+import {fetchCars} from "../CarService";
+
+describe ('Car Service', ()=> {
+    axios.defaults.baseURL = "http://localhost:3000"
+
+    const server = setupServer()
+    beforeAll(() => server.listen({onUnhandledRequest: 'error'}))
+    afterAll(() => server.close())
+    afterEach(() => server.resetHandlers())
+
+    it('should fetch cars', async () => {
+        const expected: Car[] = [
+            {id: 1, make: 'Ford', model: 'Mustang', year: 2017, price: 30000.65, used: true},
+            {id: 2, make: 'Kia', model: 'Optima', year: 2020, price: 13500, used: true},
+            {id: 3, make: 'Chevy', model: 'Silverado', year: 2025, price: 60000.34, used: false},
+        ];
+        server.use(http.get('api/carInventory', () =>
+            HttpResponse.json(expected, {status: 201})
+        ))
+        expect(await fetchCars()).toStrictEqual(expected)
+    })
+})
